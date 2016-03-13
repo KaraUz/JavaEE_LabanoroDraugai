a657706e14bc21f6ad65337371602642bb46a2d5
Karolis <Karolis@Stalinis>
2016-03-13 15:10:29 +0200

    prime faces

---
+++ src/main/java/lt/vu/mif/labanorodraugai/Reservation/SummerhousesManager.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.vu.mif.labanorodraugai.Reservation;
+
+import java.util.Date;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import lt.vu.mif.labanorodraugai.entities.House;
+import lt.vu.mif.labanorodraugai.entities.Objecttable;
+
+/**
+ *
+ * @author Karolis
+ */
+@Named
+@Stateless
+public class SummerhousesManager {
+
+}
